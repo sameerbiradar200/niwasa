1b2f2087c0d3648b3c662ac092260132e52a3749
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-09-13 15:11:31 +0530

    clean repo

--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/node_modules_933db265._.js",
-  "static/chunks/_c17825dc._.js"
-],
-    source: "dynamic"
-});
